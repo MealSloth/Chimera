99ba562f29ccc13e971658be63c42229759c0c29
Michael <michael@mealsloth.com>
2016-02-15 15:13:29 -0500

    Adding blob_image_upload to urls

--- Chimera/urls.py
+++ Chimera/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns(
     url(r'^$', views.home, name='home'),
     url(r'^blob-image-upload/', views.blob_image_upload, name='blob-image-upload'),
     url(r'^blob-image-view/', views.blob_image_view, name='blob-image-view'),
+    url(r'^blog-image-upload/', views.blog_image_upload, name='blog-image-upload'),
     url(r'^user-model-from-id/([^/]+)/', views.user_model_from_id, name='user-model-from-id'),
     url(r'^post-model-from-id/([^/]+)/', views.post_model_from_id, name='post-model-from-id'),
     url(r'^user-model-from-email/([^/]+)/', views.user_model_from_email, name='user-model-from-email'),
