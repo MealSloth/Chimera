00eacf1d787f0ed0c2818fe6077176d619c9f0e8
Michael <michael@mealsloth.com>
2016-02-21 19:41:45 -0500

    CHIMERA-23 CHIMERA-24

--- Chimera/jobs.py
+++ Chimera/jobs.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from enums import PostStatus, OrderStatus
 from datetime import timedelta, datetime
 from models import Post, Order
@@ -6,21 +7,23 @@ from models import Post, Order
 def job_post_status():
     post_list = Post.objects.all()
     for post in post_list:
-        if datetime.utcnow() > datetime.strptime(post.expire_time, format="%Y-%m-%dT%H:%M:%S"):
+        if datetime.utcnow() > datetime.strptime(post.expire_time[:19], "%Y-%m-%d %H:%M:%S"):
             post.post_status = PostStatus.INACTIVE
             try:
                 post.save()
             except StandardError, error:
                 print(error)
+    return HttpResponseRedirect('/')
 
 
 def job_order_status():
     order_list = Order.objects.all()
     for order in order_list:
-        order_time = datetime.strptime(order.order_time, format="%Y-%m-%dT%H:%M:%S.000000")
+        order_time = datetime.strptime(order.order_time[:19], "%Y-%m-%d %H:%M:%S.000000")
         if datetime.utcnow() - order_time > timedelta(days=1):
             order.order_status = OrderStatus.DELIVERED
             try:
                 order.save()
             except StandardError, error:
                 print(error)
+    return HttpResponseRedirect('/')
