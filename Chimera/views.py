d1227d8909f02798d828b4b6b88e44a8842e63a6
Michael <michael@mealsloth.com>
2016-02-23 01:33:09 -0500

    HYDRA-8

--- Chimera/views.py
+++ Chimera/views.py
@@ -54,10 +54,18 @@ def blob_upload(request):  # /blob/upload
 # /blob/delete
 def blob_delete(request):
     if request.method == 'POST':
-        return HttpResponse(
-            urllib2.urlopen(PROTOCOL + 'blob.mealsloth.com/blob/delete/'),
-            content_type='application/json'
-        )
+        body = loads(request.body)
+        blob_id = body.get('blob_id')
+        if not blob_id:
+            response = Result.get_result_dump(Result.INVALID_PARAMETER)
+            return HttpResponse(response, content_type='application/json')
+        else:
+            data = {'blob_id': blob_id}
+            data = dumps(data)
+            return HttpResponse(
+                urllib2.urlopen(PROTOCOL + 'blob.mealsloth.com/blob/delete/', data),
+                content_type='application/json'
+            )
     else:
         response = Result.get_result_dump(Result.POST_ONLY)
         return HttpResponse(response, content_type='application/json')
