668b426d275b81b931a2c95050d6d9b7bb7e5984
Michael <michael@mealsloth.com>
2016-02-22 21:53:18 -0500

    VALKYRIE-65 #comment Adding url suffixes as enum

---
+++ Chimera/storage_url_suffixes.py
@@ -0,0 +1,15 @@
+class StorageURLSuffixes:
+    def __init__(self):
+        pass
+
+    USER_PROFILE_PHOTO = 0
+    SIREN_BLOG = 1
+
+    _storage_url_suffixes = {
+        USER_PROFILE_PHOTO: 'user/profile-photo/',
+        SIREN_BLOG: 'siren/blog/',
+    }
+
+    @staticmethod
+    def get_url_suffix(storage_url_suffix):
+        return StorageURLSuffixes._storage_url_suffixes.get(storage_url_suffix)
