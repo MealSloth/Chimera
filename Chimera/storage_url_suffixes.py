f3f3b3253a874c55c16b7bf8e5f3ed15b6faa1c8
Michael <michael@mealsloth.com>
2016-02-24 21:18:05 -0500

    Cast as an int before sending..? I don't know why

--- Chimera/storage_url_suffixes.py
+++ Chimera/storage_url_suffixes.py
@@ -6,7 +6,7 @@ class StorageURLSuffixes:
     SIREN_BLOG = 1
     POST = 2
 
-    _storage_url_suffixes = {
+    _suffixes = {
         USER_PROFILE_PHOTO: 'user/profile-photo/',
         SIREN_BLOG: 'siren/blog/',
         POST: 'post/',
@@ -14,8 +14,10 @@ class StorageURLSuffixes:
 
     @staticmethod
     def get_url_suffix(storage_url_suffix):
-        return StorageURLSuffixes._storage_url_suffixes.get(storage_url_suffix)
+        suffix = StorageURLSuffixes.get_url_suffixes().get(storage_url_suffix)
+        return suffix
 
     @staticmethod
     def get_url_suffixes():
-        return StorageURLSuffixes._storage_url_suffixes
+        suffixes = StorageURLSuffixes._suffixes
+        return suffixes
