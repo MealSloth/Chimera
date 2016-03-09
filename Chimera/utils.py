88b14362179f385f75f56c2aa553708a040413c2
Michael <michael@mealsloth.com>
2016-03-09 13:06:23 -0800

    CHIMERA-47

--- Chimera/utils.py
+++ Chimera/utils.py
@@ -8,3 +8,14 @@ def model_to_dict(model):
     for key, value in model_dictionary.iteritems():
         dictionary[key] = value
     return dictionary
+
+
+def format_phone_number(country_code, phone_number):
+    if country_code is None or country_code <= 0:
+        country_code = 1
+    country_code = "+" + country_code
+    phone_number = phone_number.replace("+", "")
+    phone_number = phone_number.replace("-", "")
+    phone_number = phone_number.replace("_", "")
+    phone_number = phone_number.replace(" ", "")
+    return country_code + "_" + phone_number
