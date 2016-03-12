5262702531f4bad7bde28a068f818194bbcc53f4
Michael <michael@mealsloth.com>
2016-03-11 17:19:38 -0800

    FREYA-15

--- Chimera/view/interaction/view_interaction.py
+++ Chimera/view/interaction/view_interaction.py
@@ -18,25 +18,35 @@ def interaction(request, **kwargs):
         interaction_id = body.get('interaction_id')
 
         if not interaction_id:
-            response = Result.get_result_dump(Result.INVALID_PARAMETER)
-            return HttpResponse(response, content_type='application/json')
+            interaction_list = Interaction.objects.all()
+            if interaction_list.count() > 0:
+                interactions = []
+                for item in interaction_list:
+                    interactions.append(model_to_dict(item))
+                response = {'interactions': interactions}
+                Result.append_result(response, Result.SUCCESS)
+                response = dumps(response)
+                return HttpResponse(response, content_type='application/json')
+            else:
+                response = Result.get_result_dump(Result.DATABASE_ENTRY_NOT_FOUND)
+                return HttpResponse(response, content_type='application/json')
+        else:
+            try:
+                current_interaction = Interaction.objects.get(pk=interaction_id)
+            except Interaction.DoesNotExist:
+                response = Result.get_result_dump(Result.DATABASE_ENTRY_NOT_FOUND)
+                return HttpResponse(response, content_type='application/json')
+            except Interaction.MultipleObjectsReturned:
+                response = Result.get_result_dump(Result.DATABASE_MULTIPLE_ENTRIES)
+                return HttpResponse(response, content_type='application/json')
 
-        try:
-            current_interaction = Interaction.objects.get(pk=interaction_id)
-        except Interaction.DoesNotExist:
-            response = Result.get_result_dump(Result.DATABASE_ENTRY_NOT_FOUND)
-            return HttpResponse(response, content_type='application/json')
-        except Interaction.MultipleObjectsReturned:
-            response = Result.get_result_dump(Result.DATABASE_MULTIPLE_ENTRIES)
-            return HttpResponse(response, content_type='application/json')
+            if kwargs:
+                return current_interaction
 
-        if kwargs:
-            return current_interaction
-
-        response = {'interaction': model_to_dict(current_interaction)}
-        Result.append_result(response, Result.SUCCESS)
-        response = dumps(response)
-        return HttpResponse(response, content_type='application/json')
+            response = {'interaction': model_to_dict(current_interaction)}
+            Result.append_result(response, Result.SUCCESS)
+            response = dumps(response)
+            return HttpResponse(response, content_type='application/json')
     else:
         response = Result.get_result_dump(Result.POST_ONLY)
         return HttpResponse(response, content_type='application/json')
