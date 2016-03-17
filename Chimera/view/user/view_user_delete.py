1cb4cb0cbf66c6bfc2fab7aaab872e26b541b3d5
Michael <michael@mealsloth.com>
2016-03-17 11:37:25 -0400

    Fixing something maybe?

--- Chimera/view/user/view_user_delete.py
+++ Chimera/view/user/view_user_delete.py
@@ -27,7 +27,7 @@ def user_delete(request, **kwargs):
             user = user[0]
         else:
             response = Result.get_result_dump(Result.DATABASE_ENTRY_NOT_FOUND)
-            return  HttpResponse(response, content_type='application/json')
+            return HttpResponse(response, content_type='application/json')
 
         try:
             user_login = UserLogin.objects.get(pk=user.user_login_id)
